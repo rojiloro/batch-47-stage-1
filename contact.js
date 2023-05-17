c162f47eea46c75e5a1899c91a183b1f7d84047f
rojiloro <kelengoji79@gmail.com>
2023-05-17 12:00:11 +0700

    belajar js end

--- contact.js
+++ contact.js
@@ -51,3 +51,58 @@
 // } else {
 //   console.log("kamu tidak lulus");
 // }
+
+// // function
+// function Hello() {
+//   let x = 5;
+//   let y = 10;
+
+//   let result = x * y;
+//   console.log(result);
+// }
+
+// Hello();
+
+// function Hello2(x, y) {
+//   console.log(x);
+//   console.log(y);
+
+//   let result = x * y;
+//   console.log(result);
+// }
+// Hello2(5, 10);
+
+// function namaSaya(name) {
+//   console.log(name);
+// }
+
+// namaSaya("oji");
+
+function submitData() {
+  let name = document.getElementById("input-name").value;
+  let email = document.getElementById("input-email").value;
+  let phone = document.getElementById("input-phone").value;
+  let subject = document.getElementById("subject").value;
+  let message = document.getElementById("input-message").value;
+
+  if (name == "") {
+    return alert("Nama Harus diisi");
+  } else if (email == "") {
+    return alert("Email harus diisi");
+  } else if (phone == "") {
+    return alert("Telepon harus diisi");
+  } else if (subject == "") {
+    return alert("Subject Harus diisi");
+  }
+
+  let emailReceiver = "kelengoji79@gmail.com";
+
+  let a = document.createElement("a");
+  a.href = `mailto:${emailReceiver}?subject=${subject}&body=Halo, Nama saya ${name}, ${message}. Silahkan kontak saya ${phone}`;
+  a.click();
+  console.log(name);
+  console.log(email);
+  console.log(phone);
+  console.log(subject);
+  console.log(message);
+}
