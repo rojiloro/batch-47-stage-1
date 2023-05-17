4c8f28d20237549a48962099b7dce6a81e482e4a
rojiloro <kelengoji79@gmail.com>
2023-05-17 10:02:59 +0700

    pengenalan js

--- contact.js
+++ contact.js
@@ -1,2 +1,34 @@
-console.log("hai student");
-alert("selamat pagi");
+// console.log("hai student");
+// alert("selamat pagi");
+// document.write("batch 47");
+
+// variabel
+
+// let, var, const
+
+// bisa di deklarasikan ulang dan bisa diubah valuenya
+// var gelas = "Air Putih";
+// var gelas = "Kopi";
+// console.log(gelas);
+
+// // tidak bisa di deklarasikan ulang, namun value/datanya bisa kita ubah
+// let pemerintah = "PDIP";
+// pemerintah = "Nasdem";
+// console.log(pemerintah);
+
+// let Pemerintah = "golkar";
+// console.log(Pemerintah);
+// // tidak bisa di deklarasikan ulang atau tidak bisa diubah value
+// const rog = "asus";
+// // rog = "lenovo";
+// console.log(rog);
+
+// tipe data
+// string, number, boolean
+let name = "oji"; //string
+let umur = 15; //number
+let isMarried = false; //boolean
+
+// nama saya oji umur saya 15 tahun
+console.log("nama saya oji umur saya 15 tahun");
+console.log("Nama saya " + name + " umur saya " + umur + " tahun");
