d0349a32856e3968a4c50c93fdfd10bdc79752fe
rojiloro <kelengoji79@gmail.com>
2023-05-24 09:38:09 +0700

    add index.js

---
+++ js/index.js
@@ -0,0 +1,11 @@
+let hamburgerIsOpen = false;
+
+function openHamburger() {
+  let hamburerNavContainer = document.getElementById("hamburger-nav-container");
+  if (!hamburgerIsOpen){
+    hamburerNavContainer.style.display = "block";
+    hamburgerIsOpen = true;  
+  }else {
+    
+  }
+}
